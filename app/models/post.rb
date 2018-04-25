4a53167e41eab1f0475f30d133b9ef0e1685c898
Benjamin Peters <Benjamin.peters@gmail.com>
2018-04-25 18:43:12 -0400

    fixed create and update models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post
   attr_reader :id, :username, :avatar, :post, :mood, :song
-  DB = PG.connect(host: "localhost", port: 5432, dbname: 'musicmood')
+  DB = PG.connect(host: "", port: 5432, dbname: 'musicmood')
 
   def initialize(opts={})
     @id = opts["id"].to_i
@@ -27,7 +27,7 @@ class Post
     results = DB.exec(
       <<-SQL
         INSERT INTO posts (username, avatar, post, mood, song)
-        VALUES ( '#{opts["username"]}','#{opts["avatar"]}','#{opts["post"]}','#{opts["mood"]}','#{opts["song"]}')
+        VALUES ('#{opts["username"]}','#{opts["avatar"]}','#{opts["post_body"]}','#{opts["mood"]}','#{opts["song"]}')
         RETURNING id, username, avatar, post, mood, song;
       SQL
     )
@@ -36,16 +36,17 @@ class Post
 
   def self.delete(id)
     results = DB.exec("DELETE FROM posts WHERE id=#{id}")
+    return {deleted: true}
   end
 
   def self.update(id, opts={})
     results = DB.exec(
       <<-SQL
         UPDATE posts
-        SET username='#{opts["username"]}', avatar='#{opts["avatar"]}',post='#{opts["post"]}',mood='#{opts["mood"]}',song='#{opts["song"]}')
+        SET username='#{opts["username"]}', avatar='#{opts["avatar"]}', post='#{opts["post_body"]}', mood='#{opts["mood"]}', song='#{opts["song"]}'
         WHERE id=#{id}
         RETURNING id, username, avatar, post, mood, song;
-         SQL
+      SQL
     )
     return Post.new(results.first)
   end
