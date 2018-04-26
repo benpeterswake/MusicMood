a80016e72f1ee103f682cfafe768f43b90806c61
Benjamin Peters <Benjamin.peters@gmail.com>
2018-04-26 19:41:37 -0400

    created css files

--- public/js/Navigation.js
+++ public/js/Navigation.js
@@ -3,7 +3,7 @@ class Navigation extends React.Component {
     return (
       <div>
         <nav className="navbar navbar-dark bg-dark fixed-top">
-            <a className="navbar-brand" href="#">
+            <a className="navbar-brand " href="#">
                 <img src="musiclogo.png" className="d-inline-block align-top" alt=""/>
             </a>
         </nav>
