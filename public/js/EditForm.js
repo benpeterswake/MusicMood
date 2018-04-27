290e8b51331842aaa0edb6579294a4f6d5e56d09
Olena Penn <olenapenn@gmail.com>
2018-04-26 14:40:32 -1000

    EditForm styling

--- public/js/EditForm.js
+++ public/js/EditForm.js
@@ -38,23 +38,26 @@ class EditForm extends React.Component{
 
     render(){
         return(
-            <div className="col-lg-12 mx-auto">
-                <h2>{this.state.song}</h2>
-                <form onSubmit={this.submitEdit}>
-                  <div className="row">
-                    <div className="col">
-                      <input type="text" className="form-control" id="username" value={this.state.username} onChange={this.handleChange} placeholder="Username" />
-                    </div>
-                    <div className="col">
-                      <input type="text" className="form-control" id="song" value={this.state.song} onChange={this.handleChange} placeholder="Song" />
-                    </div>
+            <section id="EditForm">
+             <div className="container">
+              <div className="col-lg-12 mx-auto">
+                <div className="card">
+                 <form onSubmit={this.submitEdit}>
+                  <div className="card-header">
+                   <input type="text" className="form-control" id="mood" value={this.state.mood} onChange={this.handleChange} placeholder="Mood" />
                   </div>
-                  <div className="form-group">
-                    <textarea type="text" className="form-control" id="post_body" value={this.state.post_body} onChange={this.handleChange} placeholder="Your post"></textarea>
-                  </div>
-                  <button type="submit" className="btn btn-primary">Submit Post Edits</button>
-                </form>
+
+                  <div className="card-body">
+                   <div className="form-group">
+                    <input type="text" className="form-control card-text" id="song" value={this.state.song} onChange={this.handleChange} placeholder="Song" />
+                   </div>
+                    <button type="submit" className="btn btn-primary">Submit Edits</button>
+                </div>
+               </form>
+              </div>
+             </div>
             </div>
+        </section>
         )
     }
 
