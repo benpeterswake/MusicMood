6d1e5710e512a103a4c3b214b4aeba3d287ee856
victorjaquez <victorajaquez@gmail.com>
2018-04-27 02:26:13 -0700

    initial styling for newsfeed

--- public/js/PostsList.js
+++ public/js/PostsList.js
@@ -1,46 +1,66 @@
 class PostsList extends React.Component{
-    render(){
-        return(
-          <section id="PostList">
-          <div className="container">
+  render(){
+    return(
+      <section id="PostList">
+        <div className="container">
           <div className="col-lg-6 mx-auto">
-              {
-                  this.props.posts.map((post, index) => {
-                    return (
-                      <div className="card">
-                        {/* Show list template*/}
-                        {
-                            this.props.editPost !== index ?
-                            <div>
-                            <div className="card-header">
-                              {post.mood}
-                              <button type="button" className="btn btn-default btn-sm"
-                              onClick = {() => this.props.toggleState(index, post)}>Edit</button>
-                              <button type="button" className="btn btn-default btn-sm"
-                              onClick = {() => this.props.deletePost(post, index)}>Delete</button>
+            {
+              this.props.posts.map((post, index) => {
+                return (
+
+
+                  <div className="card">
+                    {/* Show list template*/}
+                    {
+                      this.props.editPost !== index ?
+                      <div>
+
+                        <div class="card-block">
+
+                          <div className="card-subtitle">
+                            <button type="button" className="badge badge-danger float-right"
+                              onClick = {() => this.props.deletePost(post, index)}>X</button>
+
+                              <span className="pencil float-right"
+                                onClick = {() => this.props.toggleState(index, post)} >&#9998;</span>
+                              </div>
+
+                          <div className="card-header"> Mood: {post.mood}
+                              </div>
                             </div>
 
                             <div className="card-body">
-                              <p>{post.song}</p>
-                              <blockquote className="blockquote mb-0">
-                                <footer className="blockquote-footer">{post.username}</footer>
-                              </blockquote>
+                                <div className="card-title">
+                              <div className="song">{post.song}</div>
+
+
+                              <div className="footer"><blockquote className="blockquote mb-0">
+                                <footer className="username">@{post.username}</footer>
+                              </blockquote></div>
+
+                              </div>
                             </div>
-                            </div>
-                            : ''
+
+                          </div>
+                          : ''
                         }
                         {/* Show edit form template*/}
                         {
-                            this.props.editPost === index ?
-                            <EditForm closeEdit={this.props.closeEdit} handleUpdateSubmit={this.props.handleUpdateSubmit} post={this.props.post} />  : ''
+                          this.props.editPost === index ?
+                          <EditForm closeEdit={this.props.closeEdit} handleUpdateSubmit={this.props.handleUpdateSubmit} post={this.props.post} />  : ''
                         }
                       </div>
-                      )
+
+
+
+                    )
                   })
-              }
-          </div>
-          </div>
+                }
+              </div>
+
+
+            </div>
           </section>
         )
+      }
     }
-}
