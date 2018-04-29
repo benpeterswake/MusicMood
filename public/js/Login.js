2a0eb0f9d1c80dbef946d522f7ad13fc2bfacf8b
Olena Penn <olenapenn@gmail.com>
2018-04-28 20:36:54 -1000

    bind signup

--- public/js/Login.js
+++ public/js/Login.js
@@ -8,6 +8,7 @@ class Login extends React.Component {
   }
     this.getProfile = this.getProfile.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.signup = this.signup.bind(this)
 
   }
 
@@ -40,32 +41,24 @@ class Login extends React.Component {
       })
     })
     console.log('signup');
-
   }
-
   render() {
     return (
       <section id="LoginForm">
         <div className="container login-form">
           <div className="col-lg-6 mx-auto">
             <div className="card">
-
               <form onSubmit={this.getProfile}>
                 <div className="card-body">
-                    <div className="form-group font-weight-bold login-head">
-                        Log In
-                        </div>
+                    <div className="form-group font-weight-bold login-head">Log In</div>
                     <div className="form-group">
-                    <input type="text" id="login-username" className="form-control"      placeholder="username" onChange={this.handleChange}
-                     />
-                     </div>
-
-                     <div className="form-group">
-                    <input type="password" id="login-password" className="form-control" placeholder="password" onChange={this.handleChange}
-                     />
+                        <input type="text" id="login-username" className="form-control"      placeholder="username" required onChange={this.handleChange}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="password" id="login-password" className="form-control" placeholder="password" required onChange={this.handleChange}/>
                      </div>
                     <button type="submit" className="btn btn-primary">Login</button>
-                    <a href="#" className="btn btn-info" onClick={this.signup}> Create an account </a>
+                    <a href="#" className="btn btn-info" onClick={this.signup}> Create an account</a>
                 </div>
               </form>
             </div>
